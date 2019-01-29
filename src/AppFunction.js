0437b34a415f43d57c64b41f0800f97c2de6e3f8
Nate Pappenhagen <natepappenhagen@gmail.com>
2019-01-28 23:40:30 -0700

    used useEffect to reach out online/offline status and used useEffect to reach out to geolocation getCurrentPosition and made a watcher for the location

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -2,24 +2,57 @@
  * @Author: Nate
  * @Date:   2019-01-28 14:49:39
  * @Last Modified by:   Nate
- * @Last Modified time: 2019-01-28 22:28:10
+ * @Last Modified time: 2019-01-28 23:31:58
  */
 import React, { useState, useEffect } from "react";
+
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+};
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [status, setStatus] = useState(navigator.onLine);
+  const [{ latitude, longitude, speed }, setLocation] = useState(
+    initialLocationState
+  );
+  let mounted = true;
 
   useEffect(() => {
     document.title = `you have clicked ${count} times`;
     window.addEventListener("mousemove", handleMouseMove);
-
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+    navigator.geolocation.getCurrentPosition(handleGeolocation);
+    const watchId = navigator.geolocation.watchPosition(handleGeolocation);
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+      navigator.geolocation.clearWatch(watchId);
+      mounted = false;
     };
   }, [count]);
 
+  const handleGeolocation = event => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  };
+  const handleOnline = () => {
+    setStatus(true);
+  };
+  const handleOffline = () => {
+    setStatus(false);
+  };
   const handleMouseMove = event => {
     setMousePosition({
       x: event.pageX,
@@ -43,7 +76,7 @@ const App = () => {
         src={
           isOn
             ? "https://icon.now.sh/highlight/fd0"
-            : "https://icon.now.sh/highlight/aaa"
+            : "https://icon.now.sh/highlight/303030"
         }
         style={{
           height: "50px",
@@ -55,6 +88,14 @@ const App = () => {
       <h2>Mouse Position</h2>
       {JSON.stringify(mousePosition, null, 2)}
       <br />
+      <h2>Network Status</h2>
+      <p>
+        you are <strong>{status ? "online" : "offline"}</strong>
+      </p>
+      <h2>Geolocation </h2>
+      <p>latitude is {latitude ? latitude : ""}</p>
+      <p>longitude is {longitude ? longitude : ""}</p>
+      <p>speed is {speed ? speed : "0"}</p>
     </React.Fragment>
   );
 };
