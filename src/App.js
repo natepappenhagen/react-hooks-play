e1995672914a3d362420ea1365cff2c078c049b4
Nate Pappenhagen <natepappenhagen@gmail.com>
2019-01-30 10:23:18 -0700

    used useReducer to create a redux like counter example. Simple setup

--- src/App.js
+++ src/App.js
@@ -2,18 +2,55 @@
  * @Author: Nate
  * @Date:   2019-01-30 09:13:39
  * @Last Modified by:   Nate
- * @Last Modified time: 2019-01-30 09:46:26
+ * @Last Modified time: 2019-01-30 10:19:48
  */
-import React, { useContext } from "react";
-import { UserContext } from "./index";
+import React, { useReducer } from "react";
 
-export default function App({ username }) {
-  const value = useContext(UserContext);
-  return <div>Hello, {value}</div>;
+const initialState = {
+  count: 0
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return {
+        count: state.count + 1
+      };
+    case "decrement":
+      return {
+        count: state.count - 1
+      };
+    case "reset":
+      return initialState;
+    default:
+      return initialState;
+  }
 }
 
-// traditional way to use Context API
-// import { UserContext } from "./index";
-//       <UserContext.Consumer>
-//         {value => <div>Hello, {value} </div>}
-//       </UserContext.Consumer>
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="container max-w-sm mx-auto p-4 m-2 bg-teal-lightest shadow-lg rounded text-center text-orange font-bold">
+      count: {state.count}
+      <button
+        className="container mx-auto"
+        onClick={() => dispatch({ type: "increment" })}
+      >
+        increment
+      </button>
+      <button
+        className="container mx-auto"
+        onClick={() => dispatch({ type: "decrement" })}
+      >
+        decrement
+      </button>
+      <button
+        className="container mx-auto"
+        onClick={() => dispatch({ type: "reset" })}
+      >
+        reset
+      </button>
+    </div>
+  );
+}
